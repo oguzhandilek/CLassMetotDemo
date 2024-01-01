b27ca1c9b5994ac22a564c9a4ac6947146c56f1e
MONSTER <oguzhandilek25@gmail.com>
2024-01-01 09:36:17 +0300

    Add project files.

---
+++ CLassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLassMetotDemo
+{
+    public class MusteriManager : IMusteriService<Musteri>
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Name+" :Müşteri Kaydı Yapıldı");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Silme Yapıldı");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Güncelleme Yapıldı");
+        }
+    }
+}
---
+++ CLassMetotDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.Id = 1;
+            musteri.Name = "Oğuzhan";
+            musteri.LastName = "Dilek";
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri);
+
+
+            Console.ReadLine();
+        }
+    }
+}
